187595c9f6f8d071eff4b47ff8f5a497a2f0581e
Marazzo <Joseph.Marazzo@tiaa.org>
2017-07-19 16:12:44 -0400

    output.java

--- output.java
+++ output.java
@@ -1,44 +1,84 @@
-
 import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 import java.io.IOException;
 import java.io.BufferedWriter;
-import java.io.FileWriter;
 
 public class CSVReader {
 
-    private static final char DEFAULT_SEPARATOR = ',';
-    private static final char DEFAULT_QUOTE = '"';
+	private static final char DEFAULT_SEPARATOR = ',';
+	private static final char DEFAULT_QUOTE = '"';
+	private static final String inputFile  = "C:\\Users\\marazzj\\Desktop\\sample_names.csv";
+	private static final String outputFile = "C:\\Users\\marazzj\\Desktop\\test.csv";
 
-    public static void main(String[] args) throws Exception {
-    	String test="";
-        String csvFile = "C:\\Users\\marazzj\\Desktop\\sample_names.csv";
+	public static void main(String[] args) throws Exception {
+		String finalOutput = "";
+		Scanner scanner = new Scanner(new File(inputFile));
+		while (scanner.hasNext()) {
+			List<String> line = parseLine(scanner.nextLine());
+			String localCase = line.get(0);
 
-        Scanner scanner = new Scanner(new File(csvFile));
-        while (scanner.hasNext()) {
-            List<String> line = parseLine(scanner.nextLine());
-            test= test + line.get(0) + " " + "\n " ;
-            writeToFile(test);
+			if (localCase.contains(",")) {
+				int a = localCase.indexOf(",");
+				// System.out.println(a);
+				String first = localCase.substring(a);
+				String last = localCase.substring(0, a);
+				localCase = first + " " + last;
+				localCase = localCase.substring(2);
+			}
+
+			else {
+				localCase = line.get(0);
+			}
+			;
+			finalOutput = finalOutput + localCase + "" + "\n";
+			writeToFile(finalOutput);
+		}
+		scanner.close();
+
+        String csvFile = outputFile;
+
+        Scanner scanner1 = new Scanner(new File(csvFile));
+        while (scanner1.hasNext()) {
+            List<String> line = parseLine(scanner1.nextLine());
+            
+            String name = line.get(0);           
+            int start = name.indexOf(' ');
+            int end = name.lastIndexOf(' ');
+
+            String firstName = "";
+            String middleName = "";
+            String lastName = "";
+
+            if (start >= 0) {
+                firstName = name.substring(0, start);
+                if (end > start)
+                    middleName = name.substring(start + 1, end);
+                lastName = name.substring(end + 1, name.length());
+            }    
+            System.out.println("Full Name: "+ line.get(0));
+            System.out.println("First: " + firstName);
+            System.out.println("Middle: " + middleName);
+            System.out.println("Last: " + lastName);
+            
+            //System.out.println(line.get(0));
+    
         }
         scanner.close();
 
-    }
-    
-    public static void writeToFile(String line){
-    	BufferedWriter bw = null;
+	}
+
+	public static void writeToFile(String line) {
+		BufferedWriter bw = null;
 		FileWriter fw = null;
 
 		try {
 
-			fw = new FileWriter("C:\\Users\\marazzj\\Desktop\\test.csv");
+			fw = new FileWriter(outputFile);
 			bw = new BufferedWriter(fw);
 			bw.write(line);
-
-			System.out.println();
 
 		} catch (IOException e) {
 
@@ -63,98 +103,98 @@ public class CSVReader {
 		}
 
 	}
-    
 
-    public static List<String> parseLine(String cvsLine) {
-        return parseLine(cvsLine, DEFAULT_SEPARATOR, DEFAULT_QUOTE);
-    }
+	public static List<String> parseLine(String cvsLine) {
+		return parseLine(cvsLine, DEFAULT_SEPARATOR, DEFAULT_QUOTE);
+	}
 
-    public static List<String> parseLine(String cvsLine, char separators) {
-        return parseLine(cvsLine, separators, DEFAULT_QUOTE);
-    }
+	public static List<String> parseLine(String cvsLine, char separators) {
+		return parseLine(cvsLine, separators, DEFAULT_QUOTE);
+	}
 
-    public static List<String> parseLine(String cvsLine, char separators, char customQuote) {
+	public static List<String> parseLine(String cvsLine, char separators,
+			char customQuote) {
 
-        List<String> result = new ArrayList<>();
+		List<String> result = new ArrayList<>();
 
-        //if empty, return!
-        if (cvsLine == null && cvsLine.isEmpty()) {
-            return result;
-        }
+		// if empty, return!
+		if (cvsLine == null && cvsLine.isEmpty()) {
+			return result;
+		}
 
-        if (customQuote == ' ') {
-            customQuote = DEFAULT_QUOTE;
-        }
+		if (customQuote == ' ') {
+			customQuote = DEFAULT_QUOTE;
+		}
 
-        if (separators == ' ') {
-            separators = DEFAULT_SEPARATOR;
-        }
+		if (separators == ' ') {
+			separators = DEFAULT_SEPARATOR;
+		}
 
-        StringBuffer curVal = new StringBuffer();
-        boolean inQuotes = false;
-        boolean startCollectChar = false;
-        boolean doubleQuotesInColumn = false;
+		StringBuffer curVal = new StringBuffer();
+		boolean inQuotes = false;
+		boolean startCollectChar = false;
+		boolean doubleQuotesInColumn = false;
 
-        char[] chars = cvsLine.toCharArray();
+		char[] chars = cvsLine.toCharArray();
 
-        for (char ch : chars) {
+		for (char ch : chars) {
 
-            if (inQuotes) {
-                startCollectChar = true;
-                if (ch == customQuote) {
-                    inQuotes = false;
-                    doubleQuotesInColumn = false;
-                } else {
+			if (inQuotes) {
+				startCollectChar = true;
+				if (ch == customQuote) {
+					inQuotes = false;
+					doubleQuotesInColumn = false;
+				} else {
 
-                    //Fixed : allow "" in custom quote enclosed
-                    if (ch == '\"') {
-                        if (!doubleQuotesInColumn) {
-                            curVal.append(ch);
-                            doubleQuotesInColumn = true;
-                        }
-                    } else {
-                        curVal.append(ch);
-                    }
+					// Fixed : allow "" in custom quote enclosed
+					if (ch == '\"') {
+						if (!doubleQuotesInColumn) {
+							curVal.append(ch);
+							doubleQuotesInColumn = true;
+						}
+					} else {
+						curVal.append(ch);
+					}
 
-                }
-            } else {
-                if (ch == customQuote) {
+				}
+			} else {
+				if (ch == customQuote) {
 
-                    inQuotes = true;
+					inQuotes = true;
 
-                    //Fixed : allow "" in empty quote enclosed
-                    if (chars[0] != '"' && customQuote == '\"') {
-                        curVal.append('"');
-                    }
+					// Fixed : allow "" in empty quote enclosed
+					if (chars[0] != '"' && customQuote == '\"') {
+						curVal.append('"');
+					}
 
-                    //double quotes in column will hit this!
-                    if (startCollectChar) {
-                        curVal.append('"');
-                    }
+					// double quotes in column will hit this!
+					if (startCollectChar) {
+						curVal.append('"');
+					}
 
-                } else if (ch == separators) {
+				} else if (ch == separators) {
 
-                    result.add(curVal.toString());
+					result.add(curVal.toString());
 
-                    curVal = new StringBuffer();
-                    startCollectChar = false;
+					curVal = new StringBuffer();
+					startCollectChar = false;
 
-                } else if (ch == '\r') {
-                    //ignore LF characters
-                    continue;
-                } else if (ch == '\n') {
-                    //the end, break!
-                    break;
-                } else {
-                    curVal.append(ch);
-                }
-            }
+				} else if (ch == '\r') {
+					// ignore LF characters
+					continue;
+				} else if (ch == '\n') {
+					// the end, break!
+					break;
+				} else {
+					curVal.append(ch);
+				}
+			}
 
-        }
+		}
 
-        result.add(curVal.toString());
+		result.add(curVal.toString());
 
-        return result;
-    }
+		return result;
+	}
 
 }
